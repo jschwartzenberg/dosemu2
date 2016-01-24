a4e3547f248e6037fe79ec70a9db6672537cefa7
Stas Sergeev <stsp@users.sourceforge.net>
2016-01-24 21:46:04 +0300

    signal: dont reset fault_cnt when jumping to dosemu context
    Reset it only if we exit the dpmi coopthread and really going
    to dosemu. Fault should not happen when handling dpmi_fault().

--- src/arch/linux/async/signal.c
+++ src/arch/linux/async/signal.c
@@ -936,6 +936,17 @@ static void async_awake(void *arg)
 
 static int saved_fc;
 
+void signal_switch_to_dosemu(void)
+{
+  saved_fc = fault_cnt;
+  fault_cnt = 0;
+}
+
+void signal_switch_to_dpmi(void)
+{
+  fault_cnt = saved_fc;
+}
+
 void signal_return_to_dosemu(void)
 {
   int err;
@@ -962,8 +973,6 @@ void signal_return_to_dosemu(void)
   err = sigaltstack(&ss, NULL);
   if (err)
     perror("sigaltstack");
-  saved_fc = fault_cnt;
-  fault_cnt = 0;
 #if SIGALTSTACK_WA
   setmcontext(&hack);
 #endif
@@ -971,7 +980,6 @@ void signal_return_to_dosemu(void)
 
 void signal_return_to_dpmi(void)
 {
-  fault_cnt = saved_fc;
 }
 
 void signal_set_altstack(stack_t *stk)
